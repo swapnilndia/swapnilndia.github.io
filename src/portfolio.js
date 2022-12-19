9b2cfeda6ed186039f73f49a70661aa3b56513c3
Swapnil Katiyar <swapnil240695@gmail.com>
2022-12-19 14:29:39 +0530

    improved

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const seo = {
   og: {
     title: "Swapnil Katiyar Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "https://swapnilndia.web.app/#/",
   },
 };
 
